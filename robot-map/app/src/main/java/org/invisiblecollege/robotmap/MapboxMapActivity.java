4a74dee07fb6a7a488b3a90a53be7986058dc544
Adrian Laurenzi <adrian.laurenzi@gmail.com>
2015-11-24 11:26:06 -0800

    Step 2: setup the Mapbox SDK to display a simple map

--- robot-map/app/src/main/java/org/invisiblecollege/robotmap/MapboxMapActivity.java
+++ robot-map/app/src/main/java/org/invisiblecollege/robotmap/MapboxMapActivity.java
@@ -3,12 +3,24 @@ package org.invisiblecollege.robotmap;
 import android.app.Activity;
 import android.os.Bundle;
 
+import com.mapbox.mapboxsdk.constants.Style;
+import com.mapbox.mapboxsdk.geometry.LatLng;
+import com.mapbox.mapboxsdk.views.MapView;
+
 
 public class MapboxMapActivity extends Activity {
+
+    private MapView mapView = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mapbox_map);
+
+        MapView mapView = (MapView) findViewById(R.id.mapboxMapView);
+        mapView.setStyleUrl(Style.MAPBOX_STREETS);
+        mapView.setCenterCoordinate(new LatLng(47.605967, -122.334539));
+        mapView.setZoomLevel(11);
+        mapView.onCreate(savedInstanceState);
     }
 }
