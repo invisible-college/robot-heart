e01da4bdea405df523efc10ca00066d79817a0da
Adrian Laurenzi <adrian.laurenzi@gmail.com>
2015-11-25 12:23:16 -0800

    Step 3: setup the Google Maps SDK to display a simple map

---
+++ robot-map/app/src/main/java/org/invisiblecollege/robotmap/GoogleMapsActivity.java
@@ -0,0 +1,29 @@
+package org.invisiblecollege.robotmap;
+
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+
+public class GoogleMapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_google_maps);
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap map) {
+        // Center the map on Seattle, WA
+        map.moveCamera(CameraUpdateFactory.newLatLng(new LatLng(47.605967, -122.334539)));
+        map.moveCamera(CameraUpdateFactory.zoomTo(11));
+    }
+}
