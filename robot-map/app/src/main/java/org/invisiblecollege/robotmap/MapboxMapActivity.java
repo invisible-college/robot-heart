f229ffce2d3b02ed8b009b1603a6ae4c26ef52f3
Adrian Laurenzi <adrian.laurenzi@gmail.com>
2015-11-25 13:16:28 -0800

    Step 4: plot user location on the map

--- robot-map/app/src/main/java/org/invisiblecollege/robotmap/MapboxMapActivity.java
+++ robot-map/app/src/main/java/org/invisiblecollege/robotmap/MapboxMapActivity.java
@@ -3,6 +3,7 @@ package org.invisiblecollege.robotmap;
 import android.app.Activity;
 import android.os.Bundle;
 
+import com.mapbox.mapboxsdk.constants.MyLocationTracking;
 import com.mapbox.mapboxsdk.constants.Style;
 import com.mapbox.mapboxsdk.geometry.LatLng;
 import com.mapbox.mapboxsdk.views.MapView;
@@ -21,6 +22,16 @@ public class MapboxMapActivity extends Activity {
         mapView.setStyleUrl(Style.MAPBOX_STREETS);
         mapView.setCenterCoordinate(new LatLng(47.605967, -122.334539));
         mapView.setZoomLevel(11);
+
+        // Track user's location and display it on the map
+        mapView.setMyLocationEnabled(true);
+
+        // Disable continuously tracking user location
+        mapView.setMyLocationTrackingMode(MyLocationTracking.TRACKING_NONE);
+        
+        // Continue to track the user location and update the position on the map
+        //mapView.setMyLocationTrackingMode(MyLocationTracking.TRACKING_FOLLOW);
+
         mapView.onCreate(savedInstanceState);
     }
 }
