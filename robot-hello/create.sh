154706ee3e0420c6aac7bc1d8488791368a08dd9
Paul Pham <paul@codefellows.com>
2015-11-27 00:36:52 -0700

    Initial commit of Robot Hello project.

---
+++ robot-hello/create.sh
@@ -0,0 +1,3 @@
+# This is the shell script to create the initial Android project.
+~/src/android-sdk/tools/android create project --target 3 --name RobotHello --path ~/src/robot-heart/robot-hello --activity HelloActivity --package college.invisible.robot.hello
+
