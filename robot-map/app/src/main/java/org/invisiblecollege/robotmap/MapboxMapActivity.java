2df9bf1c80b60906119bb1100ffd57b0cc29832a
Adrian Laurenzi <adrian.laurenzi@gmail.com>
2015-11-28 18:15:35 -0800

    Step 6: allow user to drop pins following their location

--- robot-map/app/src/main/java/org/invisiblecollege/robotmap/MapboxMapActivity.java
+++ robot-map/app/src/main/java/org/invisiblecollege/robotmap/MapboxMapActivity.java
@@ -1,24 +1,31 @@
 package org.invisiblecollege.robotmap;
 
 import android.app.Activity;
+import android.location.Location;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
+import com.mapbox.mapboxsdk.annotations.MarkerOptions;
 import com.mapbox.mapboxsdk.constants.MyLocationTracking;
 import com.mapbox.mapboxsdk.constants.Style;
 import com.mapbox.mapboxsdk.geometry.LatLng;
 import com.mapbox.mapboxsdk.views.MapView;
 
+import java.util.List;
+
 
 public class MapboxMapActivity extends Activity {
 
-    private MapView mapView = null;
+    private MapView mapView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mapbox_map);
 
-        MapView mapView = (MapView) findViewById(R.id.mapboxMapView);
+        mapView = (MapView) findViewById(R.id.mapboxMapView);
+
         mapView.setStyleUrl(Style.MAPBOX_STREETS);
         mapView.setCenterCoordinate(new LatLng(47.605967, -122.334539));
         mapView.setZoomLevel(11);
@@ -32,6 +39,21 @@ public class MapboxMapActivity extends Activity {
         // Continue to track the user location and update the position on the map
         //mapView.setMyLocationTrackingMode(MyLocationTracking.TRACKING_FOLLOW);
 
+        final Button button = (Button) findViewById(R.id.saveLocationButton);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Location myLocation = mapView.getMyLocation();
+                addPointToMap(new LatLng(myLocation.getLatitude(), myLocation.getLongitude()));
+            }
+        });
+
         mapView.onCreate(savedInstanceState);
+    }
+
+    private void addPointToMap(LatLng locationToAdd) {
+        String latLngAsString = locationToAdd.getLatitude() + "," + locationToAdd.getLongitude();
+        mapView.addMarker(new MarkerOptions()
+                .position(new LatLng(locationToAdd.getLatitude(), locationToAdd.getLongitude()))
+                .title(latLngAsString));
     }
 }
