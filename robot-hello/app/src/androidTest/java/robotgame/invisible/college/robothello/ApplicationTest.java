59af164374c5a24c4cb4c21adafa7431ec840188
Paul Pham <paul@codefellows.com>
2016-01-25 22:15:36 -0600

    Add new auto-generated files for a blank project.

---
+++ robot-hello/app/src/androidTest/java/robotgame/invisible/college/robothello/ApplicationTest.java
@@ -0,0 +1,13 @@
+package robotgame.invisible.college.robothello;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
